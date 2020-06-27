2a8924524989506f670d02d4ae55256809813c95
pedroaf0 <pedroaf1233@gmail.com>
2020-06-27 18:55:21 -0300

    Update ImgGet.js

--- src/ImgGet.js
+++ src/ImgGet.js
@@ -5,24 +5,19 @@ const fs = require('fs');
 
 
 
-const ImgGet = () => new Promise(async(re,err)=>{
-
-    try{
-      request("https://ifrs.edu.br/sertao/assistencia-estudantil/restaurante/cardapio/", function (error, response, body) {
-        // console.log(body)
-         const $ = cheerio.load(body);
-         imgsrc = $('body > section > div > div.col-12.col-lg-10 > main > article > div:nth-child(3) > div > div > p > img').get()[0].attribs['data-src']
-         const file = fs.createWriteStream("./img/original/img.jpg");
-         const request = https.get(imgsrc, function(response) {
-           response.pipe(file);
-           re();
-     
-     }) 
-
-})
-    }catch(e){
-      ImgGet();
-    }       
+const ImgGet = () => new Promise((re,err)=>{
+  request("https://ifrs.edu.br/sertao/assistencia-estudantil/restaurante/cardapio/", function (error, response, body) {
+    // console.log(body)
+     const $ = cheerio.load(body);
+     imgsrc = $('body > section > div > div.col-12.col-lg-10 > main > article > div:nth-child(3) > div > div > p > img').get()[0].attribs['data-src']
+     const file = fs.createWriteStream("./img/original/img.jpg");
+     const request = https.get(imgsrc, function(response) {
+       response.pipe(file);
+ 
+ })
+ 
+ })
+       
 });
 
 module.exports.ImgGet = ImgGet;
