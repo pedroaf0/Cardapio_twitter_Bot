65c4e413a947b3894c0a4ac34be83525171c5ab5
pedroaf0 <pedroaf1233@gmail.com>
2020-07-27 10:58:38 -0300

    Update imgGet.js

--- src/ImgGet.js
+++ src/ImgGet.js
@@ -1,23 +1,18 @@
-
 var request = require("request");
-var cheerio = require('cheerio');
 var http = require('https');
 var fs = require('fs');
 
 module.exports.ImgGet = ()=> new Promise((re,err)=>{
-request("https://ifrs.edu.br/sertao/assistencia-estudantil/restaurante/cardapio/", function (error, response, body) {
-   // console.log(body)
-    const $ = cheerio.load(body);
-    imgsrc = $('body > section > div > div.col-12.col-lg-10 > main > article > div:nth-child(3) > div > div > p > img').get()[0].attribs['data-src']
-   console.log(imgsrc)
-   download(imgsrc, './img/original/img.jpg', re)
+request("https://ifrs.edu.br/sertao/wp-json/wp/v2/media?search=cardapio", function (error, response, body) {
+   console.log(JSON.parse(body)[0].guid.rendered)
+   download(JSON.parse(body)[0].guid.rendered, './img/original/img.jpg', re)
 
 })
 })
 
 const download = function(url, dest, cb) {
   var file = fs.createWriteStream(dest);
-  var request = http.get(url, function(response) {
+   http.get(url, function(response) {
     response.pipe(file);
     file.on('finish', function() {
       file.close(cb);  // close() is async, call cb after close completes.
