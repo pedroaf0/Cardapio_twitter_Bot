2e0a5c15081ed8ca9dc17366438d2c012cb3345c
pedroaf0 <pedroaf1233@gmail.com>
2020-07-27 10:59:28 -0300

    Create testes.js

---
+++ testes.js
@@ -0,0 +1,13 @@
+// Use esse arquivo para testar os modulos separadamente
+
+const { ImgGet } = require('./src/ImgGet');
+//ImgGet()
+
+const { crop } = require('./src/croper');
+//crop()
+
+const { recognize } = require('./src/recognize');
+//recognize()
+
+const { GetWeather } = require('./src/GetWeather');
+//GetWeather()
